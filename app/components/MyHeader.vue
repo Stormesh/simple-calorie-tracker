0ceaf82871d163391659873d6096161c462413a1
Stormesh <Androashraf6478@gmail.com>
2025-08-01 02:13:55 +0300

    Added Nutritionix Logo

--- app/components/MyHeader.vue
+++ app/components/MyHeader.vue
@@ -1,5 +1,6 @@
 <template>
-  <header class="flex items-center px-10 bg-stone-50 shadow-md h-16">
+  <header class="flex justify-between items-center px-10 bg-stone-50 shadow-md h-16">
     <NuxtImg src="maxhp_side.png" alt="Logo" class="h-16" />
+    <NutritionixLogo />
   </header>
 </template>
---
+++ app/components/NutritionixLogo.vue
@@ -0,0 +1,3 @@
+<template>
+    <NuxtImg src="Updated_NutritionixAPI_hires_flat.png" alt="Nutritionix" class="h-16" />
+</template>
