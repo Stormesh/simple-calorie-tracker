bbd3ff6b3a5d4a4ccb581b4805f0ac758cab7b3b
Stormesh <Androashraf6478@gmail.com>
2025-08-04 13:11:48 +0300

    Fixed a bug related to nuxt/ui not being available

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ["@nuxt/ui", "@nuxt/eslint", "@nuxt/image", "@nuxt/icon"],
+  modules: ["@nuxt/eslint", "@nuxt/image", "@nuxt/icon"],
   css: ["~/assets/css/app.css"],
   runtimeConfig: {
     public: {
