b582dfa5c05947d09f670727b84c00d3589cd36b
Stormesh <Androashraf6478@gmail.com>
2026-09-24 16:16:37 +0300

    Enhance accessibility in FatSecret's logo

--- app/components/FatSecretLogo.vue
+++ app/components/FatSecretLogo.vue
@@ -1,8 +1,9 @@
 <template>
-  <NuxtLink href="https://www.fatsecret.com">
+  <NuxtLink href="https://www.fatsecret.com" aria-label="FatSecret">
     <img
       src="https://platform.fatsecret.com/api/static/images/powered_by_fatsecret.svg"
       border="0"
+      alt="FatSecret"
     />
   </NuxtLink>
 </template>
